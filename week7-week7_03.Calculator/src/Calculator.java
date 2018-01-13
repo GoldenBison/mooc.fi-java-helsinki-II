295e5fb3b94b91edf0a285a83b2fab2799a4663e
Horace Kuan <horacekuan@gmail.com>
2018-01-13 22:01:37 +1100

    Complete "Calculator"

--- week7-week7_03.Calculator/src/Calculator.java
+++ week7-week7_03.Calculator/src/Calculator.java
@@ -1,24 +1,43 @@
 public class Calculator {
     private Reader reader;
+    private int numOfCalculations;
     
     public Calculator() {
         reader = new Reader();
+        numOfCalculations = 0;
+    }
+    
+    private int[] getValues() {
+        int[] values = new int[2];
+        
+        System.out.print("value1: ");
+        values[0] = reader.readInteger();
+        System.out.print("value2: ");
+        values[1] = reader.readInteger();
+        
+        return values;
     }
     
     private void sum() {
-        
+        int[] values = getValues();
+        int sum = values[0] + values[1];
+        System.out.println("sum of the values " + sum);
     }
     
     private void difference() {
-        
+        int[] values = getValues();
+        int difference = values[0] - values[1];
+        System.out.println("difference of the values " + difference);
     }
     
     private void product() {
-        
+        int[] values = getValues();
+        int product = values[0] * values[1];
+        System.out.println("product of the values " + product);
     }
     
     private void statistics() {
-        
+        System.out.println("Calculations done " + numOfCalculations);
     }
     
     public void start() {
@@ -32,10 +51,13 @@ public class Calculator {
 
             if (command.equals("sum")) {
                 sum();
+                numOfCalculations++;
             } else if (command.equals("difference")) {
                 difference();
+                numOfCalculations++;
             } else if (command.equals("product")) {
                 product();
+                numOfCalculations++;
             }
         }
 
