ce50f8edf00fb75afa5e943d9215adbdad12e6a1
Horace Kuan <horacekuan@gmail.com>
2018-01-13 16:26:17 +1100

    Complete "Character String Changer"

---
+++ week7-week7_02.CharacterStringChanger/src/Changer.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Changer {
+    private ArrayList<Change> changeList;
+    
+    public Changer() {
+        changeList = new ArrayList<Change>();
+    }
+    
+    public void addChange(Change change) {
+        changeList.add(change);
+    }
+    
+    public String change(String characterString) {
+        for (Change changeItem : changeList) {
+            characterString = changeItem.change(characterString);
+        }
+        
+        return characterString;
+    }
+}
