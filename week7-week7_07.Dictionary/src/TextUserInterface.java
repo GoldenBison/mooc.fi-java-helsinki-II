76b6ecf4de50984037f96ef37fa885c1d050f06d
Horace Kuan <horacekuan@gmail.com>
2018-01-24 22:40:32 +1100

    Complete "Dictionary"

---
+++ week7-week7_07.Dictionary/src/TextUserInterface.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class TextUserInterface {
+    private Scanner reader;
+    private Dictionary dictionary;
+    
+    public TextUserInterface(Scanner reader, Dictionary dictionary) {
+        this.reader = reader;
+        this.dictionary = dictionary;
+    }
+    
+    public void start() {
+        System.out.println("Statement:");
+        System.out.println("  quit - quit the text user interface\n");
+        
+        while (true) {
+            String input = reader.nextLine();
+            if (input.equals("quit")) {
+                System.out.println("Cheers!");
+                break;
+            } else if (input.equals("add")) {
+                System.out.print("In Finnish: ");
+                String word = reader.nextLine();
+                System.out.print("Translation: ");
+                String translation = reader.nextLine();
+                
+                add(word, translation);
+            } else if (input.equals("translate")) {
+                System.out.print("Give a word: ");
+                String word = reader.nextLine();
+                System.out.println("Translation: " + translate(word));
+            } else {
+                System.out.println("Unknown statement\n");
+            }
+        }
+    }
+    
+    public void add(String word, String translation) {
+        dictionary.add(word, translation);
+    }
+    
+    public String translate(String word) {
+        return dictionary.translate(word);
+    }
+}
