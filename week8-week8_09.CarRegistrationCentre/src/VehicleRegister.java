baf2f51dd1a4f23215d495c3b3edcb769c9d9934
Horace Kuan <horacekuan@gmail.com>
2018-01-29 20:38:28 +1100

    Complete "CarRegistrationCentre"

---
+++ week8-week8_09.CarRegistrationCentre/src/VehicleRegister.java
@@ -0,0 +1,53 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class VehicleRegister {
+    private HashMap<RegistrationPlate, String> owners;
+    
+    public VehicleRegister() {
+        owners = new HashMap<RegistrationPlate, String>();
+    }
+    
+    public boolean add(RegistrationPlate plate, String owner) {
+        if (owners.containsKey(plate)) {
+            return false;
+        }
+        
+        owners.put(plate, owner);
+        
+        return true;
+    }
+    
+    public String get(RegistrationPlate plate) {
+        if (!owners.containsKey(plate)) {
+            return null;
+        }
+        
+        return owners.get(plate);
+    }
+    
+    public boolean delete(RegistrationPlate plate) {
+        if (!owners.containsKey(plate)) {
+            return false;
+        }
+        
+        owners.remove(plate);
+        
+        return true;
+    }
+    
+    public void printRegistrationPlates() {
+        for (RegistrationPlate plate : owners.keySet()) {
+            System.out.println(plate);
+        }
+    }
+    
+    public void printOwners() {
+        Set<String> ownerSet = new HashSet<String>(owners.values());
+        
+        for (String owner : ownerSet) {
+            System.out.println(owner);
+        }
+    }
+}
