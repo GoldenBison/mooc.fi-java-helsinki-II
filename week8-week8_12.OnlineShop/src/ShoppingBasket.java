f31a621d17f28a934d61757eef18397146a254a8
Horace Kuan <horacekuan@gmail.com>
2018-02-06 19:08:58 +1100

    Complete "OnlineShop"

---
+++ week8-week8_12.OnlineShop/src/ShoppingBasket.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ShoppingBasket {
+    private Map<String, Purchase> purchases;
+    
+    public ShoppingBasket() {
+        purchases = new HashMap<String, Purchase>();
+    }
+    
+    public void add(String product, int price) {
+        if (purchases.containsKey(product)) {
+            purchases.get(product).increaseAmount();
+        } else {
+            purchases.put(product, new Purchase(product, 1, price));
+        }
+    }
+    
+    public int price() {
+        int total = 0;
+        
+        for (Purchase p : purchases.values()) {
+            total += p.price();
+        }
+        
+        return total;
+    }
+    
+    public void print() {
+        for (Purchase p : purchases.values()) {
+            System.out.println(p);
+        }
+    }
+}
