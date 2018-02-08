775ebfbf6b2fc7f42305d7be2939d94b3f7f255a
Horace Kuan <horacekuan@gmail.com>
2018-02-08 17:05:03 +1100

    Randomly generate jump length and votes

--- week8-week8_16.SkiJumping/src/Round.java
+++ week8-week8_16.SkiJumping/src/Round.java
@@ -3,8 +3,21 @@ import java.util.Random;
 public class Round {
 
     private Random random;
+    private int[] votes;
     
     public Round() {
         random = new Random();
+        votes = new int[5];
+    }
+    
+    public int getLength() {
+        // Random int between 60 - 120
+        return random.nextInt(61) + 1;
+    }
+    
+    public void generateVotes() {
+        for (int i = 0; i < votes.length; i++) {
+            votes[i] = random.nextInt(11) + 10;
+        }
     }
 }
