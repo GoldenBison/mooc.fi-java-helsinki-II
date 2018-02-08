ede5bea396ae7c0a38d58bd045eca525f7cda3e6
Horace Kuan <horacekuan@gmail.com>
2018-02-08 16:28:45 +1100

    Create "Participant" class
    - Stores scores in a list

---
+++ week8-week8_16.SkiJumping/src/Participant.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Participant {
+    
+    private String name;
+    private List<Integer> scores;
+    
+    public Participant(String name) {
+        this.name = name;
+        this.scores = new ArrayList<Integer>();
+    }
+    
+    
+}
