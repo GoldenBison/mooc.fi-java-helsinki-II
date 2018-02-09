1987f76e231cd0e65164beba4210c5fcd4aa688b
Horace Kuan <horacekuan@gmail.com>
2018-02-09 13:02:53 +1100

    Sort votes and calculate score for round

--- week8-week8_16.SkiJumping/src/Round.java
+++ week8-week8_16.SkiJumping/src/Round.java
@@ -1,23 +1,40 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class Round {
 
     private Random random;
     private int[] votes;
+    private int length;
     
     public Round() {
         random = new Random();
         votes = new int[5];
+        // Random int between 60 - 120
+        length = random.nextInt(61) + 1;
     }
     
     public int getLength() {
-        // Random int between 60 - 120
-        return random.nextInt(61) + 1;
+        return length;
     }
     
     public void generateVotes() {
         for (int i = 0; i < votes.length; i++) {
             votes[i] = random.nextInt(11) + 10;
         }
+    }
+    
+    public void sortVotes() {
+        Arrays.sort(votes);
+    }
+    
+    public int calculateScore() {
+        int total = 0;
+        
+        for (int i = 1; i < votes.length - 1; i++) {
+            total += votes[i];
+        }
+        
+        return total + length;
     }
 }
