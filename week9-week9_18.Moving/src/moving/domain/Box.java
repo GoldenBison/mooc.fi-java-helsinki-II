b345359f4987c269302594d1d522ad9afaa9f5d2
Horace Kuan <horacekuan@gmail.com>
2018-02-12 19:16:16 +1100

    Implement "Box" class

---
+++ week9-week9_18.Moving/src/moving/domain/Box.java
@@ -0,0 +1,38 @@
+package moving.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box implements Thing {
+
+    private int maximumCapacity;
+    private List<Thing> things;
+
+    public Box(int maximumCapacity) {
+        this.maximumCapacity = maximumCapacity;
+        this.things = new ArrayList<Thing>();
+    }
+    
+    public boolean addThing(Thing thing) {
+        if (this.getVolume() + thing.getVolume() > maximumCapacity) {
+            return false;
+        }
+        
+        things.add(thing);
+        
+        return true;
+    }
+
+    @Override
+    public int getVolume() {
+        int volume = 0;
+        
+        for (Thing t : things) {
+            volume += t.getVolume();
+        }
+        
+        return volume;
+    }
+    
+    
+}
