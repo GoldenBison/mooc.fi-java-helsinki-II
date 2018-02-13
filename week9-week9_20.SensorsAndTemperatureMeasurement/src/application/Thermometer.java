b414041fdf5d03b0cfbabf53e63269667802af6a
Horace Kuan <horacekuan@gmail.com>
2018-02-13 22:17:44 +1100

    Implement "Thermometer" class

---
+++ week9-week9_20.SensorsAndTemperatureMeasurement/src/application/Thermometer.java
@@ -0,0 +1,38 @@
+package application;
+
+import java.util.Random;
+
+public class Thermometer implements Sensor {
+
+    private boolean on;
+    
+    public Thermometer() {
+        on = false;
+    }
+    
+    @Override
+    public boolean isOn() {
+        return on;
+    }
+
+    @Override
+    public void on() {
+        on = true;
+    }
+
+    @Override
+    public void off() {
+        on = false;
+    }
+
+    @Override
+    public int measure() {
+        if (on) {
+            Random random = new Random();
+            return random.nextInt(61) - 30;
+        } else {
+            throw new IllegalStateException();
+        }
+    }
+    
+}
