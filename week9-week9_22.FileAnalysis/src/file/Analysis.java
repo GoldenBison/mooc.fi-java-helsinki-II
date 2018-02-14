3b470f0d91111c9c5e993f3d10422e1a0de51cbb
Horace Kuan <horacekuan@gmail.com>
2018-02-14 22:43:15 +1100

    Complete "File Analysis"

---
+++ week9-week9_22.FileAnalysis/src/file/Analysis.java
@@ -0,0 +1,38 @@
+package file;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Analysis {
+    
+    private File file;
+    
+    public Analysis(File file) {
+        this.file = file;
+    }
+    
+    public int lines() throws Exception {
+        Scanner reader = new Scanner(file, "UTF-8");
+        int nLines = 0;
+        
+        while (reader.hasNext()) {
+            nLines++;
+            reader.nextLine();
+        }
+        
+        return nLines;
+    }
+    
+    public int characters() throws Exception {
+        Scanner reader = new Scanner(file, "UTF-8");
+        int nChars = 0;
+        
+        while (reader.hasNext()) {
+            String line = reader.nextLine();
+            nChars += line.length() + 1;  // +1 for line breaks
+        }
+        
+        return nChars;
+    }
+}
