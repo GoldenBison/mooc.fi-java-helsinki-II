53c75d5d65335026750d3196fe1a0564a9168e21
Horace Kuan <horacekuan@gmail.com>
2018-02-18 18:55:26 +1100

    Complete "The Finnish Ringing Centre"

---
+++ week10-week10_27.TheFinnishRingingCentre/src/RingingCentre.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RingingCentre {
+
+    private Map<Bird, List<String>> observed;
+    
+    public RingingCentre() {
+        this.observed = new HashMap<Bird, List<String>>();
+    }
+    
+    public void observe(Bird bird, String place) {
+        if (!observed.containsKey(bird)) {
+            observed.put(bird, new ArrayList<String>());
+        }
+        
+        observed.get(bird).add(place);
+    }
+    
+    public void observations(Bird bird) {
+        System.out.print(bird.toString() + " observations: ");
+        
+        if (!observed.containsKey(bird)) {
+            System.out.println("0");
+        } else {
+            System.out.println(observed.get(bird).size());
+            
+            for (String place : observed.get(bird)) {
+                System.out.println(place);
+            }
+        }
+    }
+}
